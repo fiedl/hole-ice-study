ab959155c6c991d1f185649b2ac77fee3489374e
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-03-22 21:47:58 +0100

    adding script for run-time plots
    https://github.com/fiedl/hole-ice-study/issues/12

---
+++ scripts/AngularAcceptance/lib/create_runtime_plots.py
@@ -0,0 +1,50 @@
+import sys
+import pandas
+
+data_file = sys.argv[1]
+data = pandas.read_csv(data_file, delim_whitespace=True)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# prepare canvas
+fig, (ax0, ax1) = plt.subplots(2, 1, facecolor="white")
+
+#for r in data["hole_ice_radius_in_dom_radii"].unique():
+for r in [0.1, 0.5, 1.0, 2.0]:
+
+  subplot_data = data[data.hole_ice_radius_in_dom_radii == r]
+
+  x = subplot_data["effective_scattering_length"]
+  y = subplot_data["duration"] / 60 # minutes
+
+  # line plot
+  ax0.plot(x, y, label = "hole-ice radius = " + str(r) + " dom radii")
+
+  # labels
+  ax0.set(xlabel = "effective scattering length [m]", ylabel="Job execution time [min]",
+      title = "Simulation performance")
+
+ax0.grid()
+ax0.legend(loc = "upper center")
+
+
+#for s in data["effective_scattering_length"].unique():
+for s in [0.020000000000000004, 0.35, 1.0, 3.0]:
+
+  subplot_data = data[data.effective_scattering_length == s]
+
+  x = subplot_data["hole_ice_radius_in_dom_radii"]
+  y = subplot_data["duration"] / 60 # minutes
+
+  # line plot
+  ax1.plot(x, y, label = "effective scattering length = " + str(round(s, 2)) + "m")
+
+  # labels
+  ax1.set(xlabel = "hole-ice radius [dom radii]", ylabel="Job execution time [min]")
+
+ax1.grid()
+ax1.legend(loc = "upper center")
+
+
+plt.show()
