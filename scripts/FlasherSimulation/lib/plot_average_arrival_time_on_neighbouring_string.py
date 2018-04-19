95c99d3486c5ee6b3ce9a9c78be97c262aa2eacd
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-04-19 11:25:18 +0200

    flasher plot script: arrival time histogram
    https://github.com/fiedl/hole-ice-study/issues/54

---
+++ scripts/FlasherSimulation/lib/plot_average_arrival_time_on_neighbouring_string.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+# import code; code.interact(local=dict(globals(), **locals()))  # like binding.pry
+
+import sys
+import pandas
+
+# flasher data
+flasher_data_file = (sys.argv[1] if (len(sys.argv) > 1) else "~/icecube/flasher-data/oux.62_30")
+flasher_data = pandas.read_csv(flasher_data_file, delim_whitespace = True, names = ["string_number", "dom_number", "time", "charge"])
+flasher_data = flasher_data[flasher_data.string_number == 63]
+
+# simulation data
+simulation_titles = [
+  "hole-ice simulation (Run-2018-paYeiy3v, r=0.5 r_dom, esca=5cm)",
+  "hole-ice approximation (Run-2018-bi3riu7r)",
+  "no hole ice (Run-2018-Fahpon9u)"
+]
+simulation_data_files = [
+  "~/hole-ice-study/scripts/FlasherSimulation/cluster-results/Run-2018-paYeiy3v/hits.txt",
+  "~/hole-ice-study/scripts/FlasherSimulation/cluster-results/Run-2018-bi3riu7r/hits.txt",
+  "~/hole-ice-study/scripts/FlasherSimulation/cluster-results/Run-2018-Fahpon9u/hits.txt",
+]
+simulation_plot_styles = ["bo", "go", "yo"]
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# prepare canvas
+fig, ax = plt.subplots(facecolor="white")
+
+# normalize
+#sum_of_all_doms = flasher_data["charge"].sum()
+
+# plot number of hits for each receiving dom
+dom_numbers = range(1, 60)
+weighted_mean_arrival_times = []
+for dom_number in dom_numbers:
+  d = flasher_data[flasher_data.dom_number == dom_number]
+  weighted_mean_arrival_times.append((d["time"] * d["charge"]).mean())
+
+ax.plot(
+  dom_numbers,
+  weighted_mean_arrival_times,
+  "ro",
+  label = "flasher data 2012"
+)
+
+# ax.set_yscale("log")
+
+# extract relevant simulation data
+for i, simulation_title in enumerate(simulation_titles):
+  simulation_data = pandas.read_csv(simulation_data_files[i], delim_whitespace = True)
+  simulation_data = simulation_data[simulation_data.string == 63]
+
+  # normalize
+  #sum_of_all_doms = simulation_data["charge"].sum()
+
+  ax.plot(
+    sorted(simulation_data["dom"].unique()),
+    list(simulation_data.groupby(["dom"])["time"].mean()),
+    simulation_plot_styles[i],
+    label = simulation_titles[i]
+  )
+
+ax.set_title("Comparing flasher data to simulation, emitting DOM 62_30")
+ax.set_xlabel("DOM number on string 63")
+ax.set_ylabel("normalized number of hits")
+
+ax.grid()
+ax.legend(loc = "upper right")
+
+plt.show()
