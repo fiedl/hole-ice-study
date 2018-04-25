4f66a2dd5261bc2968aca868fcae32d6aa6b4b42
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-04-25 15:39:36 +0200

    flasher study: adding total number of hits in detector to performance plots
    https://github.com/fiedl/hole-ice-study/issues/57#issuecomment-384267878

--- scripts/lib/plot_simulation_time_vs_flasher_pulse_width.py
+++ scripts/lib/plot_simulation_time_vs_flasher_pulse_width.py
@@ -27,6 +27,7 @@ pulse_widths = []
 simulation_times = []
 simulation_times_minutes = []
 simulation_times_hours = []
+simulation_numbers_of_hits = []
 
 for options_file in options_files:
   options = json.load(open(options_file))
@@ -37,14 +38,25 @@ for options_file in options_files:
   simulation_times_minutes.append(duration_in_seconds / 60)
   simulation_times_hours.append(duration_in_seconds / 3600)
 
+  hits_file = os.path.join(os.path.dirname(options_file), "hits.txt")
+  simulation_data = pandas.read_csv(hits_file, delim_whitespace = True, names = ["string_number", "dom_number", "time", "charge"], header = 1)
+  total_number_of_hits_in_detector = simulation_data["charge"].sum()
+  simulation_numbers_of_hits.append(total_number_of_hits_in_detector)
 
 # prepare canvas
-fig, ax = plt.subplots(1, 1, facecolor="white")
+fig, [ax0, ax1, ax2] = plt.subplots(3, 1, facecolor="white")
 
-ax.plot(pulse_widths, simulation_times_hours, "o")
+ax0.plot(pulse_widths, simulation_times_hours, "o")
+ax0.set_xlabel("Flasher pulse width setting")
+ax0.set_ylabel("Simulation time [hours]")
+ax0.set_title("Flasher simulation performance")
 
-ax.set_xlabel("Flasher pulse width setting")
-ax.set_ylabel("Simulation time [hours]")
-ax.set_title("Flasher simulation performance")
+ax1.plot(pulse_widths, simulation_numbers_of_hits, "o")
+ax1.set_xlabel("Flasher pulse width setting")
+ax1.set_ylabel("Total number of hits in detector")
+
+ax2.plot(simulation_numbers_of_hits, simulation_times_hours, "ro")
+ax2.set_xlabel("Total number of hits in detector")
+ax2.set_ylabel("Simulation time [hours]")
 
 plt.show()
