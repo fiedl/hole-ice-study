5f63c11a08f0120af48c3f916e0cabae8c3626c8
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-05-01 15:07:00 +0200

    flasher scan: plot summed hits: add data folder as command line parameter
    https://github.com/fiedl/hole-ice-study/issues/59#issuecomment-385663912

--- scripts/lib/plot_number_of_hits_on_all_neighbouring_strings.py
+++ scripts/lib/plot_number_of_hits_on_all_neighbouring_strings.py
@@ -10,47 +10,55 @@ import pandas
 flasher_data_file = "~/icecube/flasher-data/oux.63_30"
 flasher_data = pandas.read_csv(flasher_data_file, delim_whitespace = True, names = ["string_number", "dom_number", "time", "charge"])
 
-simulation_data_file = "~/hole-ice-study/results/flasher_pulse_widths/esca0.05_r0.5rdom_abs100_width127/hits.txt"
-simulation_data = pandas.read_csv(simulation_data_file, delim_whitespace = True, names = ["string_number", "dom_number", "time", "charge"], header = 1)
+import os
+import glob2
+
+simulation_data_files = []
+if len(sys.argv) > 1:
+  base_path = sys.argv[1]
+  simulation_data_files = list(glob2.glob(os.path.join(os.path.expanduser(base_path), "./**/hits.txt")))
+if len(simulation_data_files) == 0:
+  simulation_data_files.append("~/hole-ice-study/results/flasher_pulse_widths/esca0.05_r0.5rdom_abs100_width127/hits.txt")
 
 import numpy as np
-
-# for all receiving strings, which are located around the
-# sending string 63, collect the photon hits for each
-# dom.
-#
-receiving_strings = [62, 54, 55, 64, 71, 70]
-doms = range(1, 60)
-def extract_hits(data):
-  global receiving_strings
-  global doms
-  number_of_total_hits_in_detector = data["charge"].sum()
-  hits = []
-  for dom in doms:
-    hits_at_this_z = data \
-      [data.string_number.isin(receiving_strings)] \
-      [data.dom_number == dom] \
-      ["charge"].sum()
-    hits.append(hits_at_this_z)
-  relative_hits = np.asarray(hits) * 1.0 / number_of_total_hits_in_detector
-  return relative_hits
-flasher_data_hits = extract_hits(flasher_data)
-simulation_data_hits = extract_hits(simulation_data)
-
 import matplotlib.pyplot as plt
-import numpy as np
 
-# prepare canvas
-fig, ax = plt.subplots(facecolor="white")
+for simulation_data_file in simulation_data_files:
+  simulation_data = pandas.read_csv(simulation_data_file, delim_whitespace = True, names = ["string_number", "dom_number", "time", "charge"], header = 1)
 
-ax.plot(doms, flasher_data_hits, "ro", label = "flasher data 2012, total hits = " + str(flasher_data["charge"].sum()))
-ax.plot(doms, simulation_data_hits, "bo", label = "simulation, total hits = " + str(simulation_data["charge"].sum()))
+  # for all receiving strings, which are located around the
+  # sending string 63, collect the photon hits for each
+  # dom.
+  #
+  receiving_strings = [62, 54, 55, 64, 71, 70]
+  doms = range(1, 60)
+  def extract_hits(data):
+    global receiving_strings
+    global doms
+    number_of_total_hits_in_detector = data["charge"].sum()
+    hits = []
+    for dom in doms:
+      hits_at_this_z = data \
+        [data.string_number.isin(receiving_strings)] \
+        [data.dom_number == dom] \
+        ["charge"].sum()
+      hits.append(hits_at_this_z)
+    relative_hits = np.asarray(hits) * 1.0 / number_of_total_hits_in_detector
+    return relative_hits
+  flasher_data_hits = extract_hits(flasher_data)
+  simulation_data_hits = extract_hits(simulation_data)
 
-ax.set_title("Comparing flasher data to simulation, emitting DOM 63_30")
-ax.set_xlabel("DOM number ~ z coordinate")
-ax.set_ylabel("relative number of hits")
+  # prepare canvas
+  fig, ax = plt.subplots(facecolor="white")
 
-ax.grid()
-ax.legend(loc = "upper right")
+  ax.plot(doms, flasher_data_hits, "ro", label = "flasher data 2012, total hits = " + str(flasher_data["charge"].sum()))
+  ax.plot(doms, simulation_data_hits, "bo", label = "simulation, total hits = " + str(simulation_data["charge"].sum()))
 
-plt.show()
+  ax.set_title("Comparing flasher data to simulation, emitting DOM 63_30")
+  ax.set_xlabel("DOM number ~ z coordinate")
+  ax.set_ylabel("number of hits / total number of hits in whole detector")
+
+  ax.grid()
+  ax.legend(loc = "upper right")
+
+  plt.show()
