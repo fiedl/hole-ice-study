366fa3460fd634d372c06e37fc507d8adc765ef6
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-05-14 15:43:07 +0200

    adding plot script for cross check
    https://github.com/fiedl/hole-ice-study/issues/64

---
+++ scripts/lib/plot_cross_check_photonTotalPathLength_distribution.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# See: https://github.com/fiedl/hole-ice-study/issues/64
+
+# import code; code.interact(local=dict(globals(), **locals()))  # like binding.pry
+
+import sys
+import pandas
+
+data_file = "~/hole-ice-study/scripts/FiringRange/tmp/cross_check_output.txt"
+data = pandas.read_csv(data_file, delim_whitespace = True, names = ["cross", "check", "key", "equals", "photonTotalPathLength"])
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# prepare canvas
+fig, axes = plt.subplots(1, 2, facecolor="white")
+
+for ax in axes:
+
+  # histogram the data
+  n, bins, patches = ax.hist(data["photonTotalPathLength"], bins = 50, label = "simulation data")
+
+  ## fit the exponential distribution
+  import scipy.stats as ss
+  fit_params = ss.expon.fit(data["photonTotalPathLength"])
+
+  # plot fit
+  x = np.linspace(data["photonTotalPathLength"].min(), data["photonTotalPathLength"].max(), 50)
+  y = ss.expon.pdf(x, *fit_params)
+  scale = 1.0 * sum(n) / sum(y)
+  bin_width = (data["photonTotalPathLength"].max() - data["photonTotalPathLength"].min()) / 50
+  ax.plot(x + bin_width / 2, y * scale, label = "exponential fit")
+
+  ax.set_title("Cross check #64: Photons within hole ice")
+  ax.set_xlabel("photon total path length [m]")
+
+  ax.grid()
+  ax.legend(loc = "upper right")
+
+axes[1].set_yscale("log")
+
+plt.show()
