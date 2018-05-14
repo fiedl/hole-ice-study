a2a32fd5d1b3e106a8184e6e8f4a6a12cfc2f1dd
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-05-14 16:41:13 +0200

    improve plot title
    https://github.com/fiedl/hole-ice-study/issues/64

--- scripts/lib/plot_cross_check_photonTotalPathLength_distribution.py
+++ scripts/lib/plot_cross_check_photonTotalPathLength_distribution.py
@@ -32,12 +32,13 @@ for ax in axes:
   bin_width = (data["photonTotalPathLength"].max() - data["photonTotalPathLength"].min()) / 50
   ax.plot(x + bin_width / 2, y * scale, label = 'exponential fit, $\lambda_{\mathrm{abs}}$ = ' + str(round(fit_params[1], 3)) + 'm', linewidth = 2.0)
 
-  ax.set_title("Cross check #64: Photons within hole ice")
   ax.set_xlabel("photon total path length [m]")
 
   ax.grid()
   ax.legend(loc = "upper right")
 
 axes[1].set_yscale("log")
+
+fig.suptitle("Cross check #64: Photons within hole ice", fontsize = 14)
 
 plt.show()
