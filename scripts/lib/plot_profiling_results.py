03c4e72a932a5567724d1c81795bc6a4c8e8b9c3
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-05-26 15:02:04 +0200

    plot script: profiling results
    https://github.com/fiedl/hole-ice-study/issues/69#issuecomment-392256807

---
+++ scripts/lib/plot_profiling_results.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import sys
+import pandas
+
+import os
+import glob2
+
+import matplotlib as mpl
+from matplotlib.mlab import griddata
+from matplotlib import ticker
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import code; code.interact(local=dict(globals(), **locals()))  # like binding.pry
+
+data_file = sys.argv[1]
+data = pandas.read_csv(data_file, delim_whitespace = True, names = ["PROFILING", "key", "time"])
+keys = data["key"].unique()
+
+print keys
+print data
+
+# cut strangly large values
+data = data[data.time < 1000]
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# prepare canvas
+fig, ax = plt.subplots(1, 1, facecolor="white")
+
+for key in keys:
+  times = data[data.key == key]["time"]
+  print [key, times.count()]
+
+  num_of_bins = 200
+
+  n, bins, patches = ax.hist(times, range = [0, 1000], bins = num_of_bins, label = key, alpha = 0.5)
+
+ax.set_xlabel("duration per scattering step [clock ticks]")
+ax.grid()
+ax.legend(loc = "best")
+
+#ax.set_xscale("log")
+#axes[1].set_ylim(ymax = 1e4)
+
+#fig.suptitle("Cross check #66: Photons passing through hole ice, number of photons = " + str(number_of_photons), fontsize = 14)
+
+plt.show()
+
+
+
+# prepare canvas
+fig, ax = plt.subplots(1, 1, facecolor="white")
+
+for key in keys:
+  times = data[data.key == key]["time"]
+  print [key, times.count()]
+
+  num_of_bins = 60
+
+  n, bins, patches = ax.hist(times, range = [0, 60], bins = num_of_bins, label = key, alpha = 0.5)
+
+ax.set_xlabel("duration per scattering step [clock ticks]")
+ax.grid()
+ax.legend(loc = "best")
+
+#ax.set_xscale("log")
+#axes[1].set_ylim(ymax = 1e4)
+
+#fig.suptitle("Cross check #66: Photons passing through hole ice, number of photons = " + str(number_of_photons), fontsize = 14)
+
+plt.show()
+
+
+# prepare canvas
+fig, ax = plt.subplots(1, 1, facecolor="white")
+
+# pie chart
+# https://matplotlib.org/examples/pie_and_polar_charts/pie_demo_features.html
+# labels = keys
+labels = ["add ice layers", "add hole ice", "sort", "media loop", "rest of the scattering step"]
+sizes = list(data[data.key == key]["time"].mean() for key in keys)
+explode = (0.3, 0.3, 0.3, 0.3, 0)
+
+# The last value is the total time, i.e. includes the others.
+# Thus, need to subtract the others in the pie chart.
+print sizes
+sizes[-1] = sizes[-1] - np.sum(sizes[:-1])
+print sizes
+
+ax.pie(sizes, labels=labels, explode=explode, autopct='%1.1f%%', shadow=True, startangle=90)
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.show()
+
