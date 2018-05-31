ad45b876c5ec237818eca94ff798dc1a8b931dac
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-05-31 23:03:30 +0200

    increasing resource requirements
    https://github.com/fiedl/hole-ice-study/issues/59

--- scripts/FlasherParameterScan/run.rb
+++ scripts/FlasherParameterScan/run.rb
@@ -56,8 +56,8 @@ if options[:submit_to_cluster]
   shell "qsub \\
       -l gpu \\
       -l tmpdir_size=10G \\
-      -l s_rt=5:00:00 \\
-      -l h_rss=15G \\
+      -l s_rt=11:00:00 \\
+      -l h_rss=50G \\
       -m ae \\
       -t 1-#{number_of_jobs} \\
     batch-job.sh \\
