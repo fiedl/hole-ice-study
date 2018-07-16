13e96d392b94b34dc9c328b756eb181134407e52
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-07-16 19:30:32 +0200

    arrival time distributions: use data of all doms on several receiving strings
    because there is little data, i.e. large fluctuations, with the thinning factor.
    
    https://github.com/fiedl/hole-ice-study/issues/91

--- scripts/lib/create_time_distribution_histogram.py
+++ scripts/lib/create_time_distribution_histogram.py
@@ -25,8 +25,13 @@ for data_root_folder in data_root_folders:
 
 data_dirs = list((os.path.dirname(options_file) for options_file in options_files))
 
+sending_string = 60
+sending_dom = 30
 receiving_string = 70
 receiving_dom = 30
+
+# alternative:
+receiving_strings = [70, 71, 64, 55, 62 ,54]
 
 # prepare canvas
 fig, ax = plt.subplots(1, 1, facecolor="white")
@@ -36,7 +41,7 @@ bins_array = []
 titles_array = []
 
 time_min = 500
-time_max = 3000 # ns
+time_max = 4000 # ns
 
 def str_round(number):
   return "{:.4f}".format(number)
@@ -48,7 +53,9 @@ if plot_real_flasher_data:
   flasher_data = pandas.read_csv(flasher_data_file, delim_whitespace = True, names = ["string_number", "dom_number", "time", "charge"])
   flasher_brightness = 127
   flasher_width = 127
-  receiver_data = flasher_data[flasher_data.string_number == receiving_string][flasher_data.dom_number == receiving_dom]
+  #receiver_data = flasher_data[flasher_data.string_number == receiving_string][flasher_data.dom_number == receiving_dom]
+  #receiver_data = flasher_data[flasher_data.string_number != sending_string]
+  receiver_data = flasher_data[flasher_data.string_number.isin(receiving_strings)]
   receiver_data = receiver_data[receiver_data.time < time_max][receiver_data.time > time_min] # cut
   bins = receiver_data["time"]
   weights = receiver_data["charge"]
@@ -61,7 +68,8 @@ if plot_real_flasher_data:
 for index, data_dir in enumerate(data_dirs):
   data_file = data_dir + "/hits.txt"
   data = pandas.read_csv(data_file, delim_whitespace = True, names = ["string_number", "dom_number", "time", "charge"], header = 1)
-  receiver_data = data[data.string_number == receiving_string][data.dom_number == receiving_dom]
+  #receiver_data = data[data.string_number == receiving_string][data.dom_number == receiving_dom]
+  receiver_data = data[data.string_number.isin(receiving_strings)]
   receiver_data = receiver_data[receiver_data.time < time_max][receiver_data.time > time_min] # cut
 
   options_file = os.path.join(data_dir, "./options.json")
@@ -73,7 +81,7 @@ for index, data_dir in enumerate(data_dirs):
   bins = range(time_min, time_max, bin_width)
   weights, time_edges = np.histogram(receiver_data["time"], bins + [max(bins) + bin_width])
 
-  weights = weights / simulation_options["thinning_factor"]
+  weights = weights / simulation_options.get("thinning_factor", 1.0)
 
   bins_array.append(bins)
   weights_array.append(weights)
@@ -81,15 +89,17 @@ for index, data_dir in enumerate(data_dirs):
   print simulation_options
   if "without_hole_ice" in data_dir:
     titles_array.append("Simulation without hole ice")
+  elif "standard_clsim_hole_ice_approximation" in data_dir:
+    titles_array.append("Simulation with standard-clsim hole-ice approximation")
   else:
     esca = simulation_options["effective_scattering_length"]
     r = simulation_options["hole_ice_radius_in_dom_radii"]
     titles_array.append("Simulation: $\lambda_\mathrm{e}$=" + str_round(esca) + "m, $r$=" + str_round(r) + " $r_\mathrm{DOM}$")
 
-ax.hist(bins_array, (time_max - time_min)/25/3, weights = weights_array, label = titles_array, normed = True)
+ax.hist(bins_array, (time_max - time_min) / 25 / 3, weights = weights_array, label = titles_array, normed = False, fill = False, histtype = "step")
 
 ax.set_xlabel("Photon arrival time at DOM(" + str(receiving_string) + "," + str(receiving_dom) + ") after emission at DOM (63,30) [ns]")
-ax.set_ylabel("Number of photons")
+ax.set_ylabel("Fraction of received photons [arbitrary units]")
 
 plt.legend(loc = "best")
 plt.show()
