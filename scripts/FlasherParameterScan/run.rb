220034e2c335ad7204ccfae0d092d15263faa1ef
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-07-17 20:53:31 +0200

    flasher parameter scan: configure another run with additional parameters
    https://github.com/fiedl/hole-ice-study/issues/59
    https://github.com/fiedl/hole-ice-study/issues/92

--- scripts/FlasherParameterScan/run.rb
+++ scripts/FlasherParameterScan/run.rb
@@ -19,30 +19,59 @@ log.info "README: https://github.com/fiedl/hole-ice-study/tree/master/scripts/Fl
 
 log.section "Parameter Scan Configuration"
 
-options.merge!({
-  effective_scattering_length_range: (0.01..0.10).step(0.01).collect { |v| v.round(2) } + (0.25..2.25).step(0.25).collect { |v| v.round(2) },
-  hole_ice_radius_range_in_dom_radii: [0.01] + (0.3..1.8).step(0.1).collect { |v| v.round(2) },
-  absorption_length_range: [100],
-  flasher_pulse_width_range: [127],
-  flasher_pulse_brightness_range: [127],
-  thinning_factor: 0.1
-})
+# Configure specific parameter points, i.e. pairs of esca and r
+# for the parameter scan.
+parameter_points = []
+
+# # Grid scan:
+# # https://github.com/fiedl/hole-ice-study/issues/59
+# #
+# effective_scattering_length_range = (0.01..0.10).step(0.01).collect { |v| v.round(2) } + (0.25..2.25).step(0.25).collect { |v| v.round(2) }
+# hole_ice_radius_range_in_dom_radii = [0.01] + (0.3..1.8).step(0.1).collect { |v| v.round(2) }
+# effective_scattering_length_range.each do |esca|
+#   hole_ice_radius_range_in_dom_radii.each do |r_r_dom|
+#     parameter_points << {radius_in_dom_radii: r_r_dom, esca: esca}
+#   end
+# end
+
+# 1D scan for swedish-camera radius
+# https://github.com/fiedl/hole-ice-study/issues/92
+#
+radius_in_dom_radii = 0.5
+escas = (0.001..0.01).step(0.001)
+escas.each { |esca| parameter_points << {radius_in_dom_radii: radius_in_dom_radii, esca: esca.round(3)} }
+
+# Additional points for contour plot
+# https://github.com/fiedl/hole-ice-study/issues/59#issuecomment-405671824
+#
+parameter_points << {radius_in_dom_radii: 1.4, esca: 0.4}
+parameter_points << {radius_in_dom_radii: 1.4, esca: 0.5}
+parameter_points << {radius_in_dom_radii: 1.4, esca: 0.6}
+
+parameter_points << {radius_in_dom_radii: 2.0, esca: 0.5}
+parameter_points << {radius_in_dom_radii: 2.1, esca: 0.6}
+parameter_points << {radius_in_dom_radii: 1.9, esca: 0.4}
+
 
 dom_radius = 0.16510
 mean_scattering_angle_cosine = 0.94
+
+parameter_points.each do |params|
+  params[:radius] = params[:radius_in_dom_radii] * dom_radius
+  params[:sca] = params[:esca] * (1 - mean_scattering_angle_cosine)
+end
+
 options.merge!({
-  scattering_length_range: options[:effective_scattering_length_range].collect { |s| s * ( 1 - mean_scattering_angle_cosine) },
-  hole_ice_radius_range: options[:hole_ice_radius_range_in_dom_radii].collect { |n| n * dom_radius }
+  absorption_length: 100,
+  flasher_pulse_width: 127,
+  flasher_pulse_brightness: 127,
+  thinning_factor: 0.1,
+  parameter_sets: parameter_points
 })
 
 log.configuration options
 
-number_of_jobs =
-    options[:scattering_length_range].count *
-    options[:absorption_length_range].count *
-    options[:hole_ice_radius_range].count *
-    options[:flasher_pulse_width_range].count *
-    options[:flasher_pulse_brightness_range].count
+number_of_jobs = options[:parameter_sets].count
 current_cluster_node_index = ENV['SGE_TASK_ID']
 
 
@@ -72,55 +101,52 @@ if options[:submit_to_cluster]
 else
 
   parameter_set_index = 0
-  options[:flasher_pulse_brightness_range].each do |brightness|
-    options[:flasher_pulse_width_range].each do |width|
-      options[:effective_scattering_length_range].each do |esca|
-        options[:absorption_length_range].each do |abs|
-          options[:hole_ice_radius_range_in_dom_radii].each do |radius_in_dom_radii|
+  options[:parameter_sets].each do |parameter_set|
+    parameter_set_index += 1
 
-            parameter_set_index += 1
+    radius_in_dom_radii = parameter_set[:radius_in_dom_radii]
+    esca = parameter_set[:esca]
+    abs = options[:absorption_length]
+    width = options[:flasher_pulse_width]
+    brightness = options[:flasher_pulse_brightness]
+    thinning_factor = options[:thinning_factor] || 1.0
 
-            need_to_perform_this_job = ((not current_cluster_node_index) or
-                (current_cluster_node_index.to_i == parameter_set_index))
+    need_to_perform_this_job = ((not current_cluster_node_index) or
+        (current_cluster_node_index.to_i == parameter_set_index))
 
-            if need_to_perform_this_job
-              log.section "Parameters: esca=#{esca}, abs=#{abs}, pulse_width=#{width}, brightness=#{brightness}, r=#{radius_in_dom_radii}r_dom"
+    if need_to_perform_this_job
+      log.section "Parameters: esca=#{esca}, abs=#{abs}, pulse_width=#{width}, brightness=#{brightness}, r=#{radius_in_dom_radii}r_dom"
 
-              results_directory = "results/esca#{esca}_r#{radius_in_dom_radii}rdom_abs#{abs}_width#{width}_bright#{brightness}"
+      results_directory = "results/esca#{esca}_r#{radius_in_dom_radii}rdom_abs#{abs}_width#{width}_bright#{brightness}"
 
-              if File.exists?(results_directory)
-                log.warning "Skipping already existing run #{results_directory}."
-              else
-                shell "mkdir -p #{results_directory} tmp"
-                shell "cd ../FlasherSimulation && ./run.rb \\
-                  --hole-ice=simulation \\
-                  --effective-scattering-length=#{esca} \\
-                  --absorption-length=#{abs} \\
-                  --hole-ice-radius-in-dom-radii=#{radius_in_dom_radii} \\
-                  --width=#{width} \\
-                  --brightness=#{brightness} \\
-                  --thinning-factor=#{options[:thinning_factor] || 1.0} \\
-                  > ../FlasherParameterScan/tmp/flasher_simulation.log \\
-                  2> ../FlasherParameterScan/tmp/flasher_simulation.err"
+      if File.exists?(results_directory)
+        log.warning "Skipping already existing run #{results_directory}."
+      else
+        shell "mkdir -p #{results_directory} tmp"
+        shell "cd ../FlasherSimulation && ./run.rb \\
+          --hole-ice=simulation \\
+          --effective-scattering-length=#{esca} \\
+          --absorption-length=#{abs} \\
+          --hole-ice-radius-in-dom-radii=#{radius_in_dom_radii} \\
+          --width=#{width} \\
+          --brightness=#{brightness} \\
+          --thinning-factor=#{thinning_factor} \\
+          > ../FlasherParameterScan/tmp/flasher_simulation.log \\
+          2> ../FlasherParameterScan/tmp/flasher_simulation.err"
 
-                log.ensure_file "tmp/flasher_simulation.log"
+        log.ensure_file "tmp/flasher_simulation.log"
 
-                if File.stat("tmp/flasher_simulation.err").size > 0
-                  log.error "\nThis flasher simulation script run has produced errors:"
-                  shell "cat tmp/flasher_simulation.err"
-                  shell "tail -n 20 tmp/flasher_simulation.log"
-                  log.info "\n\n"
-                end
-
-                shell "mv tmp/flasher_simulation.* #{results_directory}/"
-                shell "mv ../FlasherSimulation/results/*/* #{results_directory}/"
-
-                shell "rm #{results_directory}/*.i3"
-              end
-            end
-
-          end
+        if File.stat("tmp/flasher_simulation.err").size > 0
+          log.error "\nThis flasher simulation script run has produced errors:"
+          shell "cat tmp/flasher_simulation.err"
+          shell "tail -n 20 tmp/flasher_simulation.log"
+          log.info "\n\n"
         end
+
+        shell "mv tmp/flasher_simulation.* #{results_directory}/"
+        shell "mv ../FlasherSimulation/results/*/* #{results_directory}/"
+
+        shell "rm #{results_directory}/*.i3"
       end
     end
   end
