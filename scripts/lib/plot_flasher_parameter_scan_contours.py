f0117170d9bb75b881f7f4979542a3b4c6e47d7f
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-07-18 12:13:25 +0200

    rewrite contour plot script to use extracted helper functions from https://github.com/fiedl/hole-ice-study/issues/95
    https://github.com/fiedl/hole-ice-study/issues/59

--- scripts/lib/plot_flasher_parameter_scan_contours.py
+++ scripts/lib/plot_flasher_parameter_scan_contours.py
@@ -4,94 +4,43 @@
 
 # import code; code.interact(local=dict(globals(), **locals()))  # like binding.pry
 
-import sys
-import pandas
+import cli
+import options
 
-flasher_data_file = "~/icecube/flasher-data/oux.63_30"
-flasher_data = pandas.read_csv(flasher_data_file, delim_whitespace = True, names = ["string_number", "dom_number", "time", "charge"])
-flasher_brightness = 127
-flasher_width = 127
-
-import os
-import glob2
-
-simulation_data_folder = sys.argv[1]
-
-if not simulation_data_folder:
-  simulation_data_folder = "~/hole-ice-study/results/flasher_parameter_scan"
-simulation_data_files = list(glob2.glob(os.path.join(os.path.expanduser(simulation_data_folder), "./**/hits.txt")))
-
-import numpy as np
-import scipy, scipy.special
-
-# https://github.com/thoglu/mc_uncertainty/blob/master/llh_defs/poisson.py
-#
-def poisson_equal_weights(k,k_mc,avgweights,prior_factor=0.0):
-       return (scipy.special.gammaln((k+k_mc+prior_factor)) -scipy.special.gammaln(k+1.0)-scipy.special.gammaln(k_mc+prior_factor) + (k_mc+prior_factor)* np.log(1.0/avgweights) - (k_mc+k+prior_factor)*np.log(1.0+1.0/avgweights)).sum()
-
-# Calculate agreement of data and simulation using a likelihood function.
-# The `agreement` is a log likelihood (LLH).
-#
-def agreement(flasher_data, simulation_data, flasher_scaling, simulation_scaling):
-  #receiving_strings = [62, 54, 55, 64, 71, 70]
-  #receiving_strings = [62, 54]
-  receiving_strings = [55, 64, 71, 70]
-  doms = range(1, 60)
-
-  k = []
-  k_mc = []
-  weights = []
-  for string in receiving_strings:
-    for dom in doms:
-      data_number_of_hits = flasher_data[flasher_data.string_number == string][flasher_data.dom_number == dom]["charge"].sum()
-      simulation_number_of_hits = simulation_data[simulation_data.string_number == string][simulation_data.dom_number == dom]["charge"].sum()
-
-      if (data_number_of_hits > 0) and (simulation_number_of_hits > 0):
-        k.append(data_number_of_hits)
-        k_mc.append(simulation_number_of_hits)
-        weights.append(1.0 * flasher_scaling / simulation_scaling)
-
-  if len(k_mc) > 10:
-    return poisson_equal_weights(np.asarray(k), np.asarray(k_mc), np.asarray(weights))
-  else:
-    return -np.inf
-
-
-import json
 
 parameters_esca = []
 parameters_r_r_dom = []
-agreements = []
+llhs = []
 
-for simulation_data_file in simulation_data_files:
-  simulation_data = pandas.read_csv(simulation_data_file, delim_whitespace = True, names = ["string_number", "dom_number", "time", "charge"], header = 1)
-  simulation_options_file = os.path.join(os.path.dirname(simulation_data_file), "options.json")
-  simulation_options = json.load(open(simulation_options_file))
+for data_dir in cli.data_dirs():
+  simulation_options = options.read(data_dir)
+  llh = simulation_options["llh"]
 
-  flasher_scaling = flasher_brightness * flasher_width * 1
-  simulation_scaling = simulation_options["brightness"] * simulation_options["width"] * simulation_options["thinning_factor"]
-  a = agreement(flasher_data, simulation_data, flasher_scaling, simulation_scaling)
-
-  if a < 0:
+  if llh < 0:
     parameters_esca.append(simulation_options["effective_scattering_length"])
     parameters_r_r_dom.append(simulation_options["hole_ice_radius_in_dom_radii"])
-    agreements.append(a)
+    llhs.append(llh)
 
-# import code; code.interact(local=dict(globals(), **locals()))  # like binding.pry
 
-best_agreement = np.max(agreements)
-neg_two_delta_llhs = - 2 * (np.asarray(agreements) - best_agreement)
+import numpy as np
+
+best_llh = np.max(llhs)
+neg_two_delta_llhs = - 2 * (np.asarray(llhs) - best_llh)
 
 print("best values:")
-print("  agreement: " + str(best_agreement))
-i = np.where(agreements == best_agreement)[0][0]
+print("  LLH: " + str(best_llh))
+i = np.where(llhs == best_llh)[0][0]
 print("  esca = " + str(parameters_esca[i]) + "m")
 print("  r = " + str(parameters_r_r_dom[i]) + " r_dom")
+
 
 import matplotlib as mpl
 from matplotlib.mlab import griddata
 from matplotlib import ticker
 import matplotlib.pyplot as plt
+
+
+# import code; code.interact(local=dict(globals(), **locals()))  # like binding.pry
 
 # prepare canvas
 fig, ax = plt.subplots(1, 1, facecolor="white")
